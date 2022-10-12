44dd348951f62da46b582ce70490438d95477f81
Johnmojo <hello@johnnychai.com>
2022-10-12 20:50:52 +0800

    style: add local fonts

--- components/Navigation/Footer.tsx
+++ components/Navigation/Footer.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer>
-      <div className="space-y-2 text-center flex flex-col items-center">
-        <div className="font-sans text-sm text-gray-500">
+      <div className="space-y-2 text-center flex flex-col items-center py-24">
+        <div className="text-sm text-stashaway-darkGrey font-akkurat">
           © 2018 Asia Wealth Platform Pte Ltd
         </div>
-        <div className="font-sans text-sm text-gray-500">
+        <div className="text-sm text-stashaway-darkGrey font-akkurat">
           Licensed by the Monetary Authority of Singapore
         </div>
-        <div className="text-cyan-600 flex font-sans text-sm space-x-2">
+        <div className="text-stashaway-cyan flex font-sans text-sm space-x-2">
           <Link href="/">
             <a>Privacy</a>
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        metropolis: ["Metropolis", "sans-serif"]
+        metropolis: ["metropolis", "sans-serif"],
+        akkurat: ["akkurat", "sans-serif"]
       }
+    },
+    colors: {
+      "stashaway-white": "#ffffff",
+      "stashaway-blue": "#0e233e",
+      "stashaway-cyan": "#62b4b1",
+      "stashaway-lightBlue": "#4b84d2",
+      "stashaway-cyan": "#62b4b1",
+      "stashaway-yellow": "#928154",
+      "stashaway-lightGrey": "#f8f8f8",
+      "stashaway-mediumGrey": "#edeff1",
+      "stashaway-darkGrey": "#828282"
     }
   },
   plugins: []
