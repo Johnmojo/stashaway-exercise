1f0305afe5e317cfe53e2597166e6907e151f89b
Johnmojo <hello@johnnychai.com>
2022-10-13 16:52:59 +0800

    feat: add compare function

--- components/Card/Compare.tsx
+++ components/Card/Compare.tsx
@@ -16,17 +16,26 @@ const Compare = () => {
   ];
 
   return (
-    <div>
-      <div>
-        <p>General Investing</p>
-        <h3>StashAway Risk Index 14%</h3>
+    <div className="relative flex">
+      <div className="w-1/2 p-12 bg-stashaway-lightGrey rounded-tl-md rounded-bl-md">
+        <div className="flex flex-col space-y-2">
+          <p className="font-semibold font-metropolis text-stashaway-blue">
+            General Investing
+          </p>
+          <h3 className="text-lg font-bold font-metropolis text-stashaway-lightBlue">
+            StashAway Risk Index 14%
+          </h3>
+        </div>
       </div>
-      <div>
+      <div className="w-1/2 p-12 bg-stashaway-mediumGrey rounded-tr-md rounded-br-md">
         <Dropdown
           id="foreignBenchmark"
           options={foreignBenchmarkOption}
           selectValue={foreignBenchmark}
         />
+      </div>
+      <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-4 h-4 p-6 m-auto text-sm font-semibold rounded-full bg-stashaway-lightBlue font-metropolis text-stashaway-white">
+        VS
       </div>
     </div>
   );
--- components/Input/Dropdown.tsx
+++ components/Input/Dropdown.tsx
@@ -12,7 +12,64 @@ const Dropdown = ({ options, id, selectValue }: Props) => {
   const [focus, setFocus] = useState(options[0].value);
   const ref = useRef();
 
-  return <div> Dummy </div>;
+  const handleClickOutside = (e) => {
+    if (!ref.current.contains(e.target)) {
+      setToggle(false);
+    }
+  };
+
+  const handleClickInner = (e) => {
+    const label = e.target.getAttribute("data-label");
+    const value = e.target.getAttribute("data-value");
+    setOption(label);
+    setFocus(value);
+    // selectValue({
+    //   name: id,
+    //   value
+    // });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+
+  return (
+    <div
+      className={`border-2 rounded-sm py-4 border-stashaway-mediumGrey relative ${
+        toggle && "before:-translate-x-1/3 before:-translate-y-1/2"
+      }`}
+      id={id}
+      name={id}
+      ref={ref}
+      value={option.toLowerCase()}
+      onClick={() => setToggle(!toggle)}
+    >
+      <span className="block overflow-hidden text-ellipsis whitespace-nowrap">
+        {option}
+      </span>
+      <ul
+        className={`opacity-0 absolute w-full mt-6 ${
+          toggle &&
+          "!opacity-100 pointer-events-auto origin-top scale-100 translate-y-0"
+        }`}
+      >
+        {options.map((option, id) => (
+          <li
+            className={`p-4 cursor-auto text-left select-none ${
+              option.value === focus && "text-stashaway-blue bg-stashaway-cyan"
+            }`}
+            key={id}
+            data-value={option.value}
+            data-label={option.label}
+            onClick={handleClickInner}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Dropdown;
