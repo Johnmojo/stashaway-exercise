92aa5df703124ad8fac6e37fe1c64572e41f1738
Johnny Chai <hello@johnnychai.com>
2022-10-18 17:41:13 +0800

    feat: add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
 import { General } from "../components";
 
+import Head from "next/head";
+
 const Index: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>StashAway Demo</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Lorem Ipsum" />
+        <meta name="author" content="Johnny Chai" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <General />
     </>
   );
