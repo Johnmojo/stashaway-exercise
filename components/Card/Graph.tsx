b2e13dcba388af9bb5d0a64821650c8ffd87d98d
Johnmojo <hello@johnnychai.com>
2022-10-18 11:56:38 +0800

    fix: graph components restructure

--- components/Card/Graph.tsx
+++ components/Card/Graph.tsx
@@ -1,5 +1,7 @@
-import Highcharts from "highcharts";
+import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
+import { useState, useRef } from "react";
+import { CurrencyRow, TimeRow } from "@components/index";
 
 interface Props {
   props?: HighchartsReact.Props;
@@ -8,6 +10,30 @@ interface Props {
 }
 
 const Graph = (props: Props) => {
+  // Array for time buttons
+  const timeArray = [
+    { id: "btn1m", label: "1 month" },
+    { id: "btn6m", label: "6 month" },
+    { id: "btnYtd", label: "Year-to-date" },
+    { id: "btn1yr", label: "1 year" },
+    { id: "btn5yr", label: "5 years" },
+    { id: "btnMax", label: "Max" }
+  ];
+
+  // Array for currency buttons
+  const currencyArray = [
+    { id: "sgd", label: "SGD" },
+    { id: "usd", label: "USD" }
+  ];
+
+  // States to keep track of which button is selected
+  const [timeState, setTimeState] = useState<string | undefined>(
+    timeArray[0].id
+  );
+  const [currencyState, setCurrencyState] = useState<string | undefined>(
+    currencyArray[0].id
+  );
+
   const processedDataA: { x: number; y: number }[] = [];
   const processedDataB: { x: number; y: number }[] = [];
   const dailySwitch = "Time Series (Daily)";
@@ -57,43 +83,7 @@ const Graph = (props: Props) => {
       },
       labelStyle: {
         visibility: "hidden"
-      },
-      buttons: [
-        {
-          type: "month",
-          count: 1,
-          text: "1m",
-          title: "View 1 month"
-        },
-        {
-          type: "month",
-          count: 6,
-          text: "6m",
-          title: "View 6 months"
-        },
-        {
-          type: "ytd",
-          text: "YTD",
-          title: "View year to date"
-        },
-        {
-          type: "year",
-          count: 1,
-          text: "1y",
-          title: "View 1 year"
-        },
-        {
-          type: "year",
-          count: 5,
-          text: "5y",
-          title: "View 5 years"
-        },
-        {
-          type: "all",
-          text: "Max",
-          title: "View all"
-        }
-      ]
+      }
     },
     legend: {
       enabled: false,
@@ -115,17 +105,23 @@ const Graph = (props: Props) => {
       }
     },
     tooltip: {
+      // formatter: function () {
+      //   return '<div style="margin: 1.25rem 2rem; color: #0e233e; text-align: right;"><div style="margin-bottom:1rem; font-weight: bold; font-size: 0.875rem;">' + {this.x} + '</div><div style="color:#828282; margin-bottom:1rem; font-size: 0.875rem; font-weight: semibold;"><span style="display: inline-block; height: 10px; width: 10px; margin-right: 10px; background-color: {series.color}"></span>{point.series.name}<div style="font-weight:bold; font-size:1.125rem; color:#3884d8">{point.y}</div></div>"</div>';
+      // },
       shared: true,
       split: false,
       backgroundColor: "#ffffff",
       borderColor: "#ffffff",
-      borderRadius: 20,
       xDateFormat: "%d %b %Y",
+      dateTimeLabelFormats: {
+        day: "%b %e, %Y"
+      },
+      borderRadius: 20,
       valuePrefix: "$",
       valueSuffix: " SGD",
       useHTML: true,
       headerFormat:
-        '<div style="margin: 1.25rem 2rem; color: #0e233e; text-align: right;"><div style="margin-bottom:1rem; font-weight: bold; font-size: 0.875rem;">{point.key}</div>',
+        '<div style="margin: 1.25rem 2rem; color: #0e233e; text-align: right;"><div style="margin-bottom:1rem; font-weight: bold; font-size: 0.875rem;">{point.x:%b %e, %Y}</div>',
       pointFormat:
         '<div style="color:#828282; margin-bottom:1rem; font-size: 0.875rem; font-weight: semibold;"><span style="display: inline-block; height: 10px; width: 10px; margin-right: 10px; background-color: {series.color}"></span>{point.series.name}<div style="font-weight:bold; font-size:1.125rem; color:#3884d8">{point.y}</div></div>',
       footerFormat: "</div>"
@@ -139,7 +135,73 @@ const Graph = (props: Props) => {
       spacingBottom: 200,
       spacingLeft: 65,
       spacingRight: 60,
-      height: "50%"
+      height: "50%",
+      events: {
+        load() {
+          const chart = this,
+            xAxis = chart.xAxis[0];
+
+          const btn1m = document.getElementById("btn1m");
+          btn1m?.addEventListener("click", function () {
+            const oneMonth = 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - oneMonth,
+              points[points.length - 1].x
+            );
+          });
+
+          const btn6m = document.getElementById("btn6m");
+          btn6m?.addEventListener("click", function () {
+            const sixMonths = 6 * 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - sixMonths,
+              points[points.length - 1].x
+            );
+          });
+
+          const btnYtd = document.getElementById("btnYtd");
+          btnYtd?.addEventListener("click", function () {
+            const threeMonths = 3 * 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - threeMonths,
+              points[points.length - 1].x
+            );
+          });
+
+          const btn1yr = document.getElementById("btn1yr");
+          btn1yr?.addEventListener("click", function () {
+            const oneYear = 12 * 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - oneYear,
+              points[points.length - 1].x
+            );
+          });
+
+          const btn5yr = document.getElementById("btn5yr");
+          btn5yr?.addEventListener("click", function () {
+            const fiveYears = 12 * 5 * 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - fiveYears,
+              points[points.length - 1].x
+            );
+          });
+
+          const btnMax = document.getElementById("btnMax");
+          btnMax?.addEventListener("click", function () {
+            const oneYear = 12 * 20 * 2629800000,
+              points = chart.series[0].points;
+            xAxis.setExtremes(
+              points[points.length - 1].x - oneYear,
+              points[points.length - 1].x
+            );
+          });
+        }
+      }
     },
     xAxis: {
       minRange: 1,
@@ -156,6 +218,7 @@ const Graph = (props: Props) => {
     },
     yAxis: {
       showFirstLabel: false,
+      opposite: false,
       title: {
         text: null
       },
@@ -186,38 +249,56 @@ const Graph = (props: Props) => {
   };
 
   return (
-    <section className="relative z-0 w-full h-auto rounded-2xl bg-stashaway-blue">
-      <HighchartsReact highcharts={Highcharts} options={options} {...props} />
-      <div className="absolute z-10 space-y-3 top-16 left-16">
-        <h1 className="text-2xl font-bold font-metropolis text-stashaway-white">
-          Portfolio value based on gross returns
-        </h1>
-        <p className="font-metropolis text-stashaway-white">
-          Gross returns and exchange rates sourced from Bloomberg as of 2th May
-          2019
-        </p>
-      </div>
-      <div className="absolute left-0 right-0 z-10 flex items-start justify-center space-x-48 bottom-14">
-        <div className="flex items-start">
-          <div className="w-5 mt-2 mr-2 h-[0.20rem] bg-stashaway-lightBlue"></div>
-          <div className="text-sm font-metropolis text-stashaway-white">
-            StashAway Risk Index 14%
-          </div>
+    <>
+      <div className="flex w-full pb-4 mx-auto pt-14 max-w-screen-2xl place-content-between">
+        <div className="space-x-3">
+          <TimeRow timeInput={timeArray} passChildState={setTimeState} />
         </div>
-        <div className="flex items-start text-sm font-metropolis text-stashaway-white">
-          <div className="w-5 mt-2 mr-2 h-[0.20rem] bg-stashaway-yellow"></div>
-          <div className="space-y-1">
-            <div>40% VTSMX (Stock) + 60% VBMFX (Bond)</div>
-            <div className="opacity-50">
-              VTSMX - Vanguard Total Stock Market Index
-            </div>
-            <div className="opacity-50">
-              VTBMX - Vanguard Total Bond Market Index
-            </div>
-          </div>
+        <div className="space-x-3">
+          <CurrencyRow
+            currencyInput={currencyArray}
+            passChildState={setCurrencyState}
+          />
         </div>
       </div>
-    </section>
+      <section className="relative z-0 w-full h-auto rounded-2xl bg-stashaway-blue">
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+          constructorType={"stockChart"}
+          {...props}
+        />
+        <div className="absolute z-10 space-y-3 top-16 left-16">
+          <h1 className="text-2xl font-bold font-metropolis text-stashaway-white">
+            Portfolio value based on gross returns
+          </h1>
+          <p className="font-metropolis text-stashaway-white">
+            Gross returns and exchange rates sourced from Bloomberg as of 2th
+            May 2019
+          </p>
+        </div>
+        <div className="absolute left-0 right-0 z-10 flex items-start justify-center space-x-48 bottom-14">
+          <div className="flex items-start">
+            <div className="w-5 mt-2 mr-2 h-[0.20rem] bg-stashaway-lightBlue"></div>
+            <div className="text-sm font-metropolis text-stashaway-white">
+              StashAway Risk Index 14%
+            </div>
+          </div>
+          <div className="flex items-start text-sm font-metropolis text-stashaway-white">
+            <div className="w-5 mt-2 mr-2 h-[0.20rem] bg-stashaway-yellow"></div>
+            <div className="space-y-1">
+              <div>40% VTSMX (Stock) + 60% VBMFX (Bond)</div>
+              <div className="opacity-50">
+                VTSMX - Vanguard Total Stock Market Index
+              </div>
+              <div className="opacity-50">
+                VTBMX - Vanguard Total Bond Market Index
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- components/Card/Result.tsx
+++ components/Card/Result.tsx
@@ -7,22 +7,6 @@ const Result = () => {
   // API creds
   const rangeDayAPI = "TIME_SERIES_DAILY";
 
-  // Array for time buttons
-  const timeArray = [
-    { id: "1m", label: "1 month" },
-    { id: "6m", label: "6 month" },
-    { id: "ytd", label: "Year-to-date" },
-    { id: "1y", label: "1 year" },
-    { id: "5y", label: "5 years" },
-    { id: "max", label: "Max" }
-  ];
-
-  // Array for currency buttons
-  const currencyArray = [
-    { id: "sgd", label: "SGD" },
-    { id: "usd", label: "USD" }
-  ];
-
   // Array for benchmark buttons
   const benchmarkArray = [
     // IBM
@@ -37,12 +21,6 @@ const Result = () => {
   const [benchmarkState, setBenchmarkState] = useState<string | undefined>(
     benchmarkArray[0].value
   );
-  const [timeState, setTimeState] = useState<string | undefined>(
-    timeArray[0].id
-  );
-  const [currencyState, setCurrencyState] = useState<string | undefined>(
-    currencyArray[0].id
-  );
 
   // Import a custom hook to fetch data
   const { data } = useFetch(
@@ -56,17 +34,6 @@ const Result = () => {
           compareInput={benchmarkArray}
           passChildState={setBenchmarkState}
         />
-      </div>
-      <div className="flex w-full pb-4 mx-auto pt-14 max-w-screen-2xl place-content-between">
-        <div className="space-x-3">
-          <TimeRow timeInput={timeArray} passChildState={setTimeState} />
-        </div>
-        <div className="space-x-3">
-          <CurrencyRow
-            currencyInput={currencyArray}
-            passChildState={setCurrencyState}
-          />
-        </div>
       </div>
       <div>
         <Graph passChildData1={data} passChildData2={vtsmx} />
